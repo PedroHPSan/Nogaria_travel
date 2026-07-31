7b47ed88f4eea19b3bd7208503b8eec99f7c2347
Pedro Santos <pedropalhetasantos@gmail.com>
2026-07-31 19:13:40 -0300

    Wire 7-park roteiro data into the active trip's seed itinerary

---
+++ src/services/roteiro/index.ts
@@ -0,0 +1,7 @@
+export { MAGIC_KINGDOM_ITEMS } from './magicKingdom';
+export { EPCOT_ITEMS } from './epcot';
+export { HOLLYWOOD_STUDIOS_ITEMS } from './hollywoodStudios';
+export { ANIMAL_KINGDOM_ITEMS } from './animalKingdom';
+export { UNIVERSAL_STUDIOS_FLORIDA_ITEMS } from './universalStudiosFlorida';
+export { ISLANDS_OF_ADVENTURE_ITEMS } from './islandsOfAdventure';
+export { EPIC_UNIVERSE_ITEMS } from './epicUniverse';
